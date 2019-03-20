d86e339a8ce8a448de4a95e5988989a207699b46
Rishant Dutt <rishant.dutt96@gmail.com>
2019-03-20 23:00:46 +0530

    Strings completed.

--- basic-types.ts
+++ basic-types.ts
@@ -13,3 +13,14 @@ let octal: number = 0o744;
 
 let color: string = "blue";
 color = "red";
+
+//     template strings (surrounded by ticks, `) can span multiple lines
+let fullName: string = "Bob Bobbington";
+let age: number = 37;
+let sentence: string = `Hello my name is ${ fullName }.
+
+I'll be ${ age + 1 } years old next month.`;
+
+//     equivalent to,
+let sentenceOld: string = "Hello, my name is " + fullName + "./n/n"
+    + "I'll be " + (age + 1) + " years old next month.";
